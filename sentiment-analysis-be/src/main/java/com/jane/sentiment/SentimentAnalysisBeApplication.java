55c6b570e521070bec8fe1cad1ec850a7959c07a
Ritu Bafna <ritujane78@gmail.com>
2026-05-27 13:34:53 +0545

    create a be project without logic

---
+++ sentiment-analysis-be/src/main/java/com/jane/sentiment/SentimentAnalysisBeApplication.java
@@ -0,0 +1,13 @@
+package com.jane.sentiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalysisBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentAnalysisBeApplication.class, args);
+	}
+
+}
---
+++ sentiment-analysis-be/src/test/java/com/jane/sentiment/SentimentAnalysisBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jane.sentiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentAnalysisBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
